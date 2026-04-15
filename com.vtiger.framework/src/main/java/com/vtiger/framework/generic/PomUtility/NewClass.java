be274ffa4ea5822483195b970b949e9b251d6421
Meghana <Meghana@LAPTOP-HHQGGICM>
2026-04-15 16:29:46 +0530

    added my new gitub practice class

---
+++ com.vtiger.framework/src/main/java/com/vtiger/framework/generic/PomUtility/NewClass.java
@@ -0,0 +1,9 @@
+package com.vtiger.framework.generic.PomUtility;
+
+public class NewClass {
+	
+  public static void main(String[] args) {
+	System.out.println("this is for my gitub practice");
+}
+
+}
