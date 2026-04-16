d63fbb31253fa92d92f9a567a2b7396c05a7f8f6
priya <priya@LAPTOP-BA3R60V7>
2026-04-16 10:17:09 +0530

    new code for gitub practice

---
+++ com.vtiger.framework/src/test/java/practice_listnerSS/Newclassforpractice.java
@@ -0,0 +1,12 @@
+package practice_listnerSS;
+
+public class Newclassforpractice {
+
+	public static void main(String[] args) {
+		System.out.println("this is for gitub practice");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
